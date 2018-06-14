d1ec374a5d2565520e09835a3d461c6e704a3d67
John Fritter <fritterj@kec1130-22.engr.oregonstate.edu>
2018-06-14 14:35:26 -0700

    messing around with mongo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var defaultFeeds = require("./defaultFeeds");
 
-/*var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
 var mongoHost = process.env.MONGO_HOST;
 var mongoPort = process.env.MONGO_PORT || '27017';
@@ -16,10 +16,11 @@ var mongoURL = "mongodb://" +
   mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort +
   "/" + mongoDBName;
 
-var mongoDB = null;*/
+var mongoDB = null;
+var feedsDB;
 
 let RssFeedEmitter = require('rss-feed-emitter');
-let feeder = new RssFeedEmitter();
+var feeder = new RssFeedEmitter();
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -30,31 +31,52 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-	var feeds = [];
-    var feeditems = [];
-    var allitems = [];
-    defaultFeeds.forEach(function(feedstring){
-        feeder.add({
-            url: feedstring.feedURL
-        });
+/*var allitems = [];
+defaultFeeds.forEach(function(feedstring){
+    feeder.add({
+        url: feedstring.feedURL
     });
-    //console.log(feeder.list());
-    feeder.on('new-item', function(item){
-       // console.log(item.title);
-		allitems.push(item);
-    });
+});*/
+var allitems = [];
+
+app.use(function(req,res,next){
+    feedsDB.updateOne(
+        {"pageName":"default"},
+        {$set: {"feedURLs": []}},
+        {upsert: true}
+        );
+    for (feed in defaultFeeds){
+        feedsDB.updateOne(
+            {"pageName": "default"},
+            {$addToSet: {"feedURLs": defaultFeeds[feed].feedURL}},
+            {upsert: true}
+        );
+    }
+    next();
+});
+
 
 
 app.get('/', function (req, res, next){
-    
-	console.log(allitems); 
-    /*for (feed in defaultFeeds){
-        db.urls.updateOne(
-            {feedurl: feed},
-            {upsert: true}
-        );
-    }*/
-    res.status(200).render('createFeed', {feeds: allitems, home: true});
+    feedsDB.find({"pageName": "default"}).toArray(function(err, feedDocs){
+        if(err){
+            res.status(500).send("Error fetching feeds from DB.");
+        } else {
+            feedDocs[0].feedURLs.forEach(function(feedstring){
+                //console.log(feedstring);
+                feeder.add({
+                    url: feedstring
+                });
+            });
+            feeder.on('new-item', function(item){
+                //console.log(item);
+                allitems.push(item);
+            });
+            console.log(allitems);
+            res.status(200).render('createFeed', {feeds: allitems, home: true});
+        }
+    });
+    next();
 });
 
 /*app.post(':feedURL', function(req, res, next){});*/
@@ -63,16 +85,17 @@ app.get('*', function (req, res) {
     res.status(404).render('404',{home: false, err404: true});
 });
 
-/*MongoClient.connect(mongoURL, function(err, client){
+MongoClient.connect(mongoURL, function(err, client){
     if(err){
         throw err;
     }
     mongoDB = client.db(mongoDBName);
+    feedsDB = mongoDB.collection("feeds");
     app.listen(port, function () {
         console.log("== Server is listening on port", port);
     });
-});*/
-app.listen(port, function () {
+});
+/*app.listen(port, function () {
         console.log("== Server is listening on port", port);
-    });
+    });*/
 
